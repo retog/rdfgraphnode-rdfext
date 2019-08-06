251ddb025a148082e8f80afde6ed896a6f8558d4
Reto Gmür <reto@factsmission.com>
2019-08-06 09:42:45 +0200

    Using ES6 imports in test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
-var GraphNode = require('../js/GraphNode.js');
-var rdf = require('ext-rdflib');
+import { equal } from 'assert';
+import GraphNode from '../js/GraphNode.js';
+import { sym, graph, parse } from 'ext-rdflib';
 
 function dc(suffix) {
-    return rdf.sym("http://dublincore.org/2012/06/14/dcelements#" + suffix);
+    return sym("http://dublincore.org/2012/06/14/dcelements#" + suffix);
 }
 
 
@@ -14,12 +14,12 @@ describe('GraphNode', function () {
         it('Getting property value from local graph', function (done) {
             let dataTurtle = '@prefix dc: <http://dublincore.org/2012/06/14/dcelements#>. \n\
                 <http://example.org/> dc:title "An example".';
-            let data = rdf.graph();
-            rdf.parse(dataTurtle, data, "http://example.org/data", "text/turtle").then(
+            let data = graph();
+            parse(dataTurtle, data, "http://example.org/data", "text/turtle").then(
                 () => {
-                    let gn = GraphNode(rdf.sym("http://example.org/"), data);
+                    let gn = GraphNode(sym("http://example.org/"), data);
                     let title = gn.out(dc("title")).value;
-                    assert.equal("An example", title);
+                    equal("An example", title);
                     done();
                 }
             )
@@ -31,12 +31,12 @@ describe('GraphNode', function () {
             let dataTurtle = '@prefix dc: <http://dublincore.org/2012/06/14/dcelements#>. \n\
                 <http://example.org/> dc:title <http://example.org/title>.\n\
                 <http://example.org/sommething> dc:creator <http://example.org/title>.';
-            let data = rdf.graph();
-            rdf.parse(dataTurtle, data, "http://example.org/data", "text/turtle").then(
+            let data = graph();
+            parse(dataTurtle, data, "http://example.org/data", "text/turtle").then(
                 () => {
-                    let gn = GraphNode(rdf.sym("http://example.org/title"), data);
+                    let gn = GraphNode(sym("http://example.org/title"), data);
                     let subj = gn.in(dc("title")).value;
-                    assert.equal("http://example.org/", subj);
+                    equal("http://example.org/", subj);
                     done();
                 }
             )
@@ -61,28 +61,28 @@ describe('GraphNode', function () {
             done();
         });
         it('Fetching turtle', function (done) {
-            let gn = GraphNode(rdf.sym("http://localhost:"+app.get('port')+"/example.ttl"));
+            let gn = GraphNode(sym("http://localhost:"+app.get('port')+"/example.ttl"));
             gn.fetch().then(gn => 
                 {
                     let title = gn.out(dc("title")).value;
-                    assert.equal("Another example", title);
+                    equal("Another example", title);
                 }).then(done);
         });
         it('Fetching RDFa', function (done) {
-            let gn = GraphNode(rdf.sym("http://localhost:"+app.get('port')+"/example.html"));
+            let gn = GraphNode(sym("http://localhost:"+app.get('port')+"/example.html"));
             gn.fetch().then(gn => 
                 {
                     let title = gn.out(dc("title")).value;
-                    assert.equal("Another example", title);
+                    equal("Another example", title);
                 }).then(done);
         });
         /* Depends on support in rdflib.js such as by https://github.com/linkeddata/rdflib.js/pull/220 */
         it('Fetching n-triples', function (done) {
-            let gn = GraphNode(rdf.sym("http://localhost:"+app.get('port')+"/example.nt"));
+            let gn = GraphNode(sym("http://localhost:"+app.get('port')+"/example.nt"));
             gn.fetch().then(gn => 
                 {
                     let title = gn.out(dc("title")).value;
-                    assert.equal("Another example", title);
+                    equal("Another example", title);
                 }).then(done);
         });
         after(function(done) {
